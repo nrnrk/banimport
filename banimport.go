78b6c50e01a444f946cc478a414ce5c9c6e9f5e5
nrnrk <noriki6t@gmail.com>
2022-06-28 03:40:44 +0900

    FIX: Adjust declaration positoins for readability

--- banimport.go
+++ banimport.go
@@ -9,13 +9,45 @@ import (
 	"golang.org/x/tools/go/analysis"
 )
 
-const doc = "banimport is a tool to check import dependencies"
+const (
+	doc            = "banimport is a tool to check import dependencies"
+	configFileName = `.banimport.yaml`
+)
 
-// Analyzer is ...
-var Analyzer = &analysis.Analyzer{
-	Name: "banimport",
-	Doc:  doc,
-	Run:  run,
+var (
+	Analyzer = &analysis.Analyzer{
+		Name: "banimport",
+		Doc:  doc,
+		Run:  run,
+	}
+	configJSON = `{"pattern":{}}`
+)
+
+func init() {
+	Analyzer.Flags.StringVar(&configJSON, "config", configJSON, "config json")
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	var config *Config
+	if err := json.Unmarshal([]byte(configJSON), &config); err != nil {
+		return nil, fmt.Errorf("JSON parse error: %w", err)
+	}
+	banList := config.getBanList(pass.Pkg.Path())
+	if len(banList) == 0 {
+		return nil, nil
+	}
+	for _, f := range pass.Files {
+		for _, imported := range f.Imports {
+			pathStr, err := strconv.Unquote(imported.Path.Value)
+			if err != nil {
+				return nil, fmt.Errorf("unexpected unquote error: %w", err)
+			}
+			if banList.ban(pathStr) {
+				pass.Reportf(imported.Pos(), "import of %s prohibited", imported.Path.Value)
+			}
+		}
+	}
+	return nil, nil
 }
 
 type (
@@ -43,35 +75,4 @@ func (bl BanList) ban(target string) bool {
 		}
 	}
 	return false
-}
-
-const configFileName = `.banimport.yaml`
-
-var configJSON = `{"pattern":{}}`
-
-func init() {
-	Analyzer.Flags.StringVar(&configJSON, "config", configJSON, "config json")
-}
-
-func run(pass *analysis.Pass) (interface{}, error) {
-	var config *Config
-	if err := json.Unmarshal([]byte(configJSON), &config); err != nil {
-		return nil, fmt.Errorf("JSON parse error: %w", err)
-	}
-	banList := config.getBanList(pass.Pkg.Path())
-	if len(banList) == 0 {
-		return nil, nil
-	}
-	for _, f := range pass.Files {
-		for _, imported := range f.Imports {
-			pathStr, err := strconv.Unquote(imported.Path.Value)
-			if err != nil {
-				return nil, fmt.Errorf("unexpected unquote error: %w", err)
-			}
-			if banList.ban(pathStr) {
-				pass.Reportf(imported.Pos(), "import of %s prohibited", imported.Path.Value)
-			}
-		}
-	}
-	return nil, nil
 }
